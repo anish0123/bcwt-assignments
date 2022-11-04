3cd705e27f6ea99a1d72921c41176be0af2c249f
Anish Maharjan <anishm@metropolia.fi>
2022-11-04 15:12:21 +0200

    set up getting cat by id

--- week-two/controllers/catController.js
+++ week-two/controllers/catController.js
@@ -4,20 +4,22 @@ const catModel = require('../models/catModel');
 
 //For getting whole array of cats
 const getCats = async (req, res) => {
-    const cats = await catModel.getAllCats();
+    const cats = await catModel.getAllCats(res);
     res.json(cats);
 };
 
 //For getting a single cat and letting user know if there is none
-const getCat = (req, res) => {
+const getCat = async (req, res) => {
     //choose only one object with matching id
-   const cat =  cats.filter(cat => cat.id == req.params.catId)[0];
+   const cat =  await catModel.getCatById(res, req.params.catId);
     if(cat) {
         res.json(cat);
     } else {
         res.sendStatus(404);
     } 
 };
+
+
 const modifyCat = (req, res) => {};
 const createCat = (req, res) => {
     console.log(req.body);
--- week-two/models/catModel.js
+++ week-two/models/catModel.js
@@ -2,16 +2,29 @@
 const pool = require("../database/db");
 const promisePool = pool.promise();
 
-const getAllCats = async () => {
+const getAllCats = async (res) => {
   try {
     // TODO: do the LEFT (or INNER) JOIN to get owner's name as ownername (from wop_user table).
     const [rows] = await promisePool.query("SELECT * FROM wop_cat");
     return rows;
   } catch (e) {
+    res.status(500).send(e.message);
     console.error("error", e.message);
   }
 };
 
+const getCatById = async (res, catId) => {
+  try {
+    // TODO: do the LEFT (or INNER) JOIN to get owner's name as ownername (from wop_user table).
+    const [rows] = 
+      await promisePool.query("SELECT * FROM wop_cat WHERE cat_id = ?",[catId]);
+    return rows[0];
+  } catch (e) {
+    res.status(500).send(e.message);
+    console.error("error", e.message);
+  }
+};
 module.exports = {
   getAllCats,
+  getCatById
 };
