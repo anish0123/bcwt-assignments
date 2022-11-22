733408dff484ffa2575860ecdb35315d3ba890ec
Anish Maharjan <anishm@metropolia.fi>
2022-11-22 14:20:07 +0200

    session started

--- week4-state-management/app.js
+++ week4-state-management/app.js
@@ -4,12 +4,44 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
 
+const dummyUser={
+username: "foo",
+password: "bar",
+};
+
+let loggedIn = false;
+
+
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(cookieParser());
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({extended: true})); // for form data
 
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.get('/form', (req, res) => {
+  res.render('form');
+});
+
+app.post('/login',(req,res) => {
+//check for username/password
+console.log(req.body);
+if(req.body.username == dummyUser.username && req.body.password == dummyUser.password){
+res.redirect('/secret');
+loggedIn = true;
+}
+})
+
+app.get('/secret', (req, res) => {
+  if(loggedIn){
+    res.render('secret');
+  } else {
+    res.redirect('/form');
+  }
+ 
 });
 
 app.get('/getCookie', (req, res) => {
